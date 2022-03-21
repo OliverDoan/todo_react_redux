0fa6e33f163fe5672b381738802dbc49376202e4
OliverDoan <doanvietthuan1005@gmail.com>
2022-03-21 17:28:49 +0700

    add todo

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,17 +1,53 @@
 import { Col, Input, Row } from "antd";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { addTodo } from "../../redux/actions";
 import Todo from "../Todo";
-
 export default function TodoList() {
+  const [todoName, setTodoName] = useState("");
+  const todoList = useSelector((state) => state.todoList);
+  const dispatch = useDispatch();
+  const handleAddButtonClick = () => {
+    dispatch(
+      addTodo({
+        id: uuidv4(),
+        name: todoName,
+        completed: false,
+      })
+    );
+
+    setTodoName("");
+  };
+  const handleInputChange = (e) => {
+    console.log("handleInputChange", e.target.value);
+    setTodoName(e.target.value);
+  };
+
   return (
     <Row style={{ height: "calc(100% - 40px)" }}>
       <Col span={24} style={{ height: "calc(100% - 40px)", overflowY: "auto" }}>
-        <Todo name="Learn React" />
-        <Todo name="Learn Redux" />
-        <Todo name="Learn JavaScript" />
+        {todoList.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            name={todo.name}
+            completed={todo.completed}
+          />
+        ))}
       </Col>
       <Col span={24}>
         <Input.Group style={{ display: "flex" }} compact>
-          <Input placeholder="Add new task" />
+          <Input
+            value={todoName}
+            onChange={handleInputChange}
+            placeholder="Add new task"
+            onKeyPress={(e) => {
+              if (e.key === "Enter" && todoName) {
+                handleAddButtonClick();
+              }
+            }}
+          />
         </Input.Group>
       </Col>
     </Row>
